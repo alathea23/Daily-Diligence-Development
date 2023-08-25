7c1636f210b305d338795de00c9acb58d91bac63
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-08-25 16:15:55 -0700

    updating READme and css

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,40 @@
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner
-//HEN the current day is displayed at the top of the calendar
-//WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours of 9am to 5pm
-//WHEN I view the time blocks for that day
-//THEN each time block is color-coded to indicate whether it is in the past, present,
-//WHEN I click into a time block
-//THEN I can enter an event
-//WHEN I click the save button for that time block
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
-//THEN the saved events persist
-
-
+//playing around with local time logic
 //import dayjsPluginUTC from 'dayjs-plugin-utc';
 //import utc from 'dayjs/plugin/utc';
 //import timezone from 'dayjs/plugin/timezone';
-//dayjs.extend(utc);
-//dayjs.extend(timezone);
-//dayjs().tz('America/Toronto');
-//import dayjsPluginUTC from 'dayjs-plugin-utc'
-//import utc from "dayjs/plugin/utc";
-//import timezone from "dayjs/plugin/timezone";
-//dayjs.extend(dayjsPluginUTC)
-//var local = day.js(now).subtract(8, "hour")
-//var utc = require('dayjs/plugin/utc')
-//var timezone = require('dayjs/plugin/timezone')
 //dateFormat(date, format, localize = true) {
 // if (!localize) {
 //  return dayjs.utc(date).tz(dayjs.tz.guess()).format(format);
 // }
-// return dayjs.utc(date).local().tz(dayjs.tz.guess()).format(format);
-// },
+var hourBlocksAll = document.querySelectorAll(".time-block")
+var hourTime = document.querySelector(".hour")
+var hourTimeAll = document.querySelectorAll(".hour")
+var appointment = document.querySelector(".description")
+var appointmentsAll = document.querySelectorAll(".description")
+var header = document.querySelector("h1")
+const clearButton = document.querySelector(".clear")
+const saveButtons = document.querySelectorAll(".btn")
 
+//replaced with iterating over a for loop
+//var block8 = document.querySelector("#hour-8")
+//var block9 = document.querySelector("#hour-9")
+//var block10 = document.querySelector("#hour-10")
+//ar block11 = document.querySelector("#hour-11")
+//var block12 = document.querySelector("#hour-12")
+//var block1 = document.querySelector("#hour-1")
+//var block2 = document.querySelector("#hour-2")
+//var block3 = document.querySelector("#hour-3")
+//var block4 = document.querySelector("#hour-4")
+//var block5 = document.querySelector("#hour-5")
+//var block6 = document.querySelector("#hour-6")
 
 var today = document.querySelector("#currentDay");
 var now = dayjs().format("MMM D, h:mm:ss A");
 var nowHour = dayjs().format("H")
 console.log(nowHour)
+var headerNow = dayjs().format("MMM D")
 
-var block8 = document.querySelector("#hour-8")
-var block9 = document.querySelector("#hour-9")
-var block10 = document.querySelector("#hour-10")
-var block11 = document.querySelector("#hour-11")
-var block12 = document.querySelector("#hour-12")
-var block1 = document.querySelector("#hour-1")
-var block2 = document.querySelector("#hour-2")
-var block3 = document.querySelector("#hour-3")
-var block4 = document.querySelector("#hour-4")
-var block5 = document.querySelector("#hour-5")
-var block6 = document.querySelector("#hour-6")
-
+header.innerText = headerNow + ", Work Scheduler"
 
 var i = 1000000
 
@@ -61,73 +45,27 @@ var localT = 0
 
 today.textContent = localT;
 
-function clock() {
-  if (i > 0) {
-    i--;
-  }
-  setTimeout(clock, 1000)
-  console.log("updating time")
-  localT = dayjs().format("MMM D, h:mm:ss A")
-  today.textContent = localT
-}
 
-clock()
-
-console.log(localT.$H);
-
-//var utc = require('dayjs/plugin/utc')
-//var timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
-
-//dayjs.extend(utc)
-//dayjs.extend(timezone)
-
-//dayjs.tz.guess()
-//console.log (dayjs.tz.guess())
-var hourBlocksAll = document.querySelectorAll(".time-block")
-var hourTime = document.querySelector(".hour")
-var hourTimeAll = document.querySelectorAll(".hour")
-var appointment = document.querySelector(".description")
-var appointmentsAll = document.querySelectorAll(".description")
-const clearButton = document.querySelector(".clear")
-const saveButtons = document.querySelectorAll(".btn")
-
-
-//function for single field
+//function for single appointment field:
 //function getDOM() {
 // var storedAppointment = localStorage.getItem("appointment")
 // appointment.value = storedAppointment
 //}
 //getDOM();
 
-function getDOMall() {
-  var recall = JSON.parse(localStorage.getItem("savedAppt")) || ["", "", "", "", "", "", "", "", "", "", "",]
-  console.log(recall)
-  let i = 0
-  appointmentsAll.forEach(appt => {
-    appt.value = recall[i]
-    i++
-  })
-}
+//testing code commented out:
+//console.log(hourBlocksAll)
+//console.log(localT)
+//console.log(localT.$H)
 
-getDOMall();
-
-function clearDOM() {
-  localStorage.clear("appointment");
-  getDOMall()
-}
-
-//testing code commented out
-console.log(hourBlocksAll)
-console.log(localT)
-console.log(localT.$H)
-
-//alternate path to get hour - error was only 12 hour time
+//alternate path to get hour - error was only 12 hour time:
 //console.log(hourBlocksAll[0].firstElementChild.innerHTML)
 //console.log(hourBlocksAll[0].innerText)
 //var hour_block = hourBlocksAll[0].innerText.slice(0,-2)
 //console.log(hour_block)
 //console.log(localT.$H > hour_block)
 
+//function to change color formmating on appointment blocks by hour
 function timeSelection() {
   console.log(nowHour)
 
@@ -167,6 +105,7 @@ function timeSelection() {
 
 timeSelection()
 
+
 //updating code to iterate over a for loop
 function saveDOMall() {
   let fullhour = 8
@@ -204,29 +143,46 @@ localStorage.setItem("Hours", JSON.stringify(appointmentsAll.value))
 
 $(function () {
   console.log(5)
+
+
+  //function to update clock to be a live clock
+  function clock() {
+    if (i > 0) {
+      i--;
+    }
+    setTimeout(clock, 1000)
+    console.log("updating time")
+    localT = dayjs().format("h:mm:ss A")
+    today.textContent = "🕐: " + localT
+  }
+  // calling clock function
+  clock()
+
+  //alternate path testing - 
+  //console.log(localT.$H);
+
   //Add a listener for click events on the save button.
+  function getDOMall() {
+    var recall = JSON.parse(localStorage.getItem("savedAppt")) || ["", "", "", "", "", "", "", "", "", "", "",]
+    console.log(recall)
+    let i = 0
+    appointmentsAll.forEach(appt => {
+      appt.value = recall[i]
+      i++
+    })
+  }
+  //calling local storage to populate on page load
+  getDOMall();
+  
+  // iterating save function over all save buttons
   saveButtons.forEach(button => button.addEventListener("click", saveDOMall))
 
-
-
-
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//clearing storage to reset to new day
+  function clearDOM() {
+    localStorage.clear("appointment");
+    getDOMall()
+  }
+  
+  clearButton.addEventListener("click", clearDOM);
 });
 
-clearButton.addEventListener("click", clearDOM);
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Work Day Scheduler Starter Code
+## Title:
+
+    Daily Diligence Development by Jenny
+
+## Description:
+
+   A Simple calendar app for scheduling your workday  
+
+## Installing:
+
+    Basic hosting through GitHub repository.
+
+## Contributors:
+
+    Jenny Wannier Tarzian
+
+    StackOverflow: 
+        https://stackoverflow.com/questions/47858518/highscore-in-local-storage-javascript
+
+    StackOverflow:
+        https://codedamn.com/news/javascript/how-to-fix-typeerror-foreach-is-not-a-function-in-javascript
+
+
+## License:
+
+    N/A
+
+## URL:
+
+ https://alathea23.github.io/Daily-Diligence-Development/
+
+![Alt text](./Assets/ScreenshotDailyDilligence.png)
